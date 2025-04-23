370e307b296f612f2dd2d92191bb0308312e8f87
COQUTE <kimjiho246@naver.com>
2025-04-23 17:50:17 +0900

    회원가입 완료 & 마이 페이지

--- boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package edu.kh.project.member.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -124,5 +126,50 @@ public class MemberController {
 	@GetMapping("checkEmail") // GET 방식 요청 /member/checkEmail
 	public int checkEmail(@RequestParam("memberEmail") String memberEmail) {
 		return service.checkEmail(memberEmail);
+	}
+	
+	/** 닉네임 중복 검사
+	 * @param memberNickname
+	 * @return 0, 1
+	 */
+	@ResponseBody
+	@GetMapping("checkNickname")
+	public int checkNickname(@RequestParam("memberNickname") String memberNickname) {
+		return service.checkNickname(memberNickname);
+	}
+	
+	/** 회원가입
+	 * @param inputMember : 입력된 회원 정보(memberEmail, memberPw, memberNickname, memberTel, memberAddress(따로 배열로 받아 처리))
+	 * @param memberAddress : 입력한 주소 input 3개의 값을 배열로 전달 [우편번호, 도로명/지번주소, 상세주소]
+	 * @param ra : 리다이렉트 시 request -> session -> request로 데이터 전달하는 객체
+	 * @return 
+	 */
+	@PostMapping("signup")
+	public String signup(Member inputMember, @RequestParam("memberAddress") String[] memberAddress, RedirectAttributes ra) {
+		
+		// log.debug("inputMember: " + inputMember);
+		// log.debug("memberAddress: " + memberAddress.toString());
+		
+		// 회원가입 서비스 호출
+		int result = service.signup(inputMember, memberAddress);
+		
+		String path = null;
+		String message = null;
+		
+		if(result > 0) { // 성공 시
+			message = inputMember.getMemberNickname() + "님의 가입을 환영합니다!";
+			path = "/";
+			
+		} else { // 실패
+			message = "회원가입 실패..";
+			path = "/member/signup";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return "redirect:" + path;
+		// 성공 -> redirect:/
+		// 실패 -> redirect:signup (상대경로)
+		//		현재 주소 / member/signup (GET 방식 요청)
 	}
 }
--- boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
@@ -19,4 +19,16 @@ public interface MemberMapper {
 	 */
 	int checkEmail(String memberEmail);
 
+	/** 닉네임 중복검사
+	 * @param memberNickname
+	 * @return
+	 */
+	int checkNickname(String memberNickname);
+
+	/** 회원가입
+	 * @param member
+	 * @return
+	 */
+	int signup(Member inputMember);
+
 }
