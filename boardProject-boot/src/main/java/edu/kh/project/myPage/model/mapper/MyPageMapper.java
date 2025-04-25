1568dab3133e5c14e8c9b881f401d934e499aaf0
COQUTE <kimjiho246@naver.com>
2025-04-25 17:50:39 +0900

    프로필 이미지 설정

--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
@@ -46,5 +46,11 @@ public interface MyPageMapper {
 	 * @return
 	 */
 	public List<UploadFile> fileList(int memberNo);
+
+	/** 프로필 이미지 변경
+	 * @param member
+	 * @return
+	 */
+	public int profile(Member member);
 	
 }
