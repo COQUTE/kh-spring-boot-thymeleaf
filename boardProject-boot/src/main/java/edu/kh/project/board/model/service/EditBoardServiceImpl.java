8ff482b413eb0b1994f0c757490eadb697b628b4
COQUTE <kimjiho246@naver.com>
2025-05-08 09:44:18 +0900

    게시글 삭제

--- boardProject-boot/src/main/java/edu/kh/project/board/model/service/EditBoardServiceImpl.java
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/service/EditBoardServiceImpl.java
@@ -207,4 +207,9 @@ public class EditBoardServiceImpl implements EditBoardService {
 		
 		return result;
 	}
+	
+	@Override
+	public int boardDelete(Map<String, Integer> map) {
+		return mapper.boardDelete(map);
+	}
 }
